0595eda63ff40efca2719eb0cf45b5bef31775dc
Mohamed-Alsgheer <mohamed.alsgheer@example.com>
2026-06-11 15:03:39 +0300

    feat: configure project environment variables and package type module

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-// import { DBconnection } from './db/DBconnection.js'
+import dotenv from "dotenv";
+dotenv.config();
 import { bootstrap } from './bootstrap.js';
 import express from 'express'
 import { DBconnection } from './db/dbconnect.js';
-import dotenv from "dotenv";
 
 
 const app = express()
-const port = 3005
+const port = process.env.PORT || 3005
 bootstrap(app)
 DBconnection()
-dotenv.config();
 
 app.get("/", (req,res)=>{
     return res.send("Hello Project 2")
 })
 
 
-app.listen(3005,()=>{
+app.listen(port,()=>{
     console.log(`Server is running on port ${port}`)
 })
