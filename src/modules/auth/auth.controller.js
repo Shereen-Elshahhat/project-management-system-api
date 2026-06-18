9b22c8bd0b0ed6794eb8a3d1d881036837ef0c89
Mohamed-Alsgheer <mohamed.alsgheer@example.com>
2026-06-18 14:24:18 +0300

    polish(auth): return 401 instead of 404 on login failure

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -20,7 +20,7 @@ const Login = catchError(async(req,res,next)=>{
                 return res.status(200).json({message:"success login with token"  ,token});
             })
         }else{
-            return res.status(404).json({message:"incorrect email or password"});
+            return res.status(401).json({message:"Incorrect email or password"});
         }
 })
 
