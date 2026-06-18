cee126d1ee2c49bd55a28509a00b892d991cf24a
Mohamed-Alsgheer <mohamed.alsgheer@example.com>
2026-06-18 14:23:49 +0300

    fix(user): call next() in pre-save hook

--- db/models/user.model.js
+++ db/models/user.model.js
@@ -43,6 +43,7 @@ const userSchema = new Schema ({
 userSchema.pre("save",async function(next){
     if(!this.isModified("password")) return next()
     this.password = await bcrypt.hash(this.password,8)
+    next()
 })
 
 
